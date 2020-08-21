53e47bc8ae814c7c5c4090d5a3e8c116bca02a1a
alexey.l <aloshalit@gmail.com>
2020-08-22 01:44:45 +0300

    try to remove bottom box

--- js/render/scenes/scene.js
+++ js/render/scenes/scene.js
@@ -242,7 +242,7 @@ export class Scene extends Node {
       return;
     }
 
-    gl.bindFramebuffer(gl.FRAMEBUFFER, layer.framebuffer);
+    //gl.bindFramebuffer(gl.FRAMEBUFFER, layer.framebuffer);
 
     if (this.clear) {
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
