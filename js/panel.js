6f5248b425b5cc59696a9a14d1caae871b71ddee
alexey.l <aloshalit@gmail.com>
2020-10-26 16:34:01 +0200

    event

--- js/panel.js
+++ js/panel.js
@@ -1,18 +1,28 @@
 'use strict';
 
+const eventPanelInteract = new CustomEvent ('panelinteract' ,{
+  bubbles: true,
+  detail: { text: "test1" }
+})
+
 const e = React.createElement;
 
-export default class Panel extends React.Component {
+class Panel extends React.Component {
   constructor(props) {
     super(props);
-    alert(props.a);
+    //alert(props.a);
+    //test();
   }
 
   render() {
     return e(
       'div',
-      { class: "panel", className: "panel" },
+      { class: "panel", className: "panel", onClick: e => e.target.dispatchEvent(eventPanelInteract)  },
       'Like'
     );
   }
 }
+
+const domContainer = document.querySelector('#panel-container');
+ReactDOM.render(React.createElement(Panel), domContainer);
+//document.getElementById("panel-container").addEventListener('panelinteract', e => console.log(e.detail.text));
